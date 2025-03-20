408d263ff66a46afb64c8af11950fdd9c2014d3c
hasham14fw <hasham14fw@gmail.com>
2025-03-20 21:24:11 +0500

    Excluded .vs folder

---
+++ Form1.Designer.cs
@@ -0,0 +1,460 @@
+﻿namespace DB_Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label8 = new Label();
+            pictureBox6 = new PictureBox();
+            label5 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel2 = new Panel();
+            panel8 = new Panel();
+            panel7 = new Panel();
+            label12 = new Label();
+            panel4 = new Panel();
+            label10 = new Label();
+            panel5 = new Panel();
+            label9 = new Label();
+            panel6 = new Panel();
+            label11 = new Label();
+            panel3 = new Panel();
+            label7 = new Label();
+            label6 = new Label();
+            label4 = new Label();
+            pictureBox7 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
+            panel7.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            panel6.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(129, 449);
+            panel1.TabIndex = 0;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Elephant", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = SystemColors.ButtonHighlight;
+            label8.Location = new Point(3, 82);
+            label8.Name = "label8";
+            label8.Size = new Size(117, 29);
+            label8.TabIndex = 8;
+            label8.Text = "Director";
+            label8.Click += label8_Click;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackgroundImage = Properties.Resources.Staff;
+            pictureBox6.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox6.Location = new Point(1, 178);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(31, 29);
+            pictureBox6.TabIndex = 8;
+            pictureBox6.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(28, 418);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Logout";
+            label5.Click += label5_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(35, 187);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Staff";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(35, 224);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Students";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(35, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Dashboard";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackgroundImage = Properties.Resources.Home1_removebg_preview__1__removebg_preview;
+            pictureBox5.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox5.ErrorImage = Properties.Resources.Logout;
+            pictureBox5.Location = new Point(3, 140);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(29, 30);
+            pictureBox5.TabIndex = 4;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = Properties.Resources.student;
+            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox4.Location = new Point(3, 214);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(33, 33);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            pictureBox3.BackgroundImage = Properties.Resources.Logout;
+            pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox3.ErrorImage = Properties.Resources.Logout;
+            pictureBox3.Location = new Point(0, 418);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(29, 21);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.LOGO_removebg_preview;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(12, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(95, 76);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = Properties.Resources.profile;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(44, 13);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(134, 96);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            panel2.BackColor = Color.FromArgb(224, 224, 224);
+            panel2.Controls.Add(panel8);
+            panel2.Controls.Add(panel7);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel5);
+            panel2.Controls.Add(panel6);
+            panel2.Controls.Add(panel3);
+            panel2.Location = new Point(135, 40);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(658, 395);
+            panel2.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            panel8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel8.BackColor = Color.White;
+            panel8.Location = new Point(248, 195);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(394, 186);
+            panel8.TabIndex = 5;
+            panel8.Paint += panel8_Paint;
+            // 
+            // panel7
+            // 
+            panel7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel7.BackColor = Color.White;
+            panel7.Controls.Add(label12);
+            panel7.Location = new Point(456, 98);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(186, 87);
+            panel7.TabIndex = 4;
+            panel7.Paint += panel7_Paint;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.White;
+            label12.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.FromArgb(0, 0, 64);
+            label12.Location = new Point(3, 7);
+            label12.Name = "label12";
+            label12.Size = new Size(122, 25);
+            label12.TabIndex = 9;
+            label12.Text = "Active Staff";
+            // 
+            // panel4
+            // 
+            panel4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel4.BackColor = Color.White;
+            panel4.Controls.Add(label10);
+            panel4.Location = new Point(248, 98);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(202, 87);
+            panel4.TabIndex = 3;
+            panel4.Paint += panel4_Paint;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.White;
+            label10.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.FromArgb(0, 0, 64);
+            label10.Location = new Point(3, 7);
+            label10.Name = "label10";
+            label10.Size = new Size(159, 25);
+            label10.TabIndex = 8;
+            label10.Text = "Active Students";
+            // 
+            // panel5
+            // 
+            panel5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel5.BackColor = Color.White;
+            panel5.Controls.Add(label9);
+            panel5.Location = new Point(248, 11);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(202, 77);
+            panel5.TabIndex = 3;
+            panel5.Paint += panel5_Paint;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.FromArgb(0, 0, 64);
+            label9.Location = new Point(3, 3);
+            label9.Name = "label9";
+            label9.Size = new Size(147, 25);
+            label9.TabIndex = 7;
+            label9.Text = "Total Students";
+            // 
+            // panel6
+            // 
+            panel6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel6.BackColor = Color.White;
+            panel6.Controls.Add(label11);
+            panel6.Location = new Point(456, 11);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(186, 77);
+            panel6.TabIndex = 3;
+            panel6.Paint += panel6_Paint;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.White;
+            label11.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.FromArgb(0, 0, 64);
+            label11.Location = new Point(3, 3);
+            label11.Name = "label11";
+            label11.Size = new Size(110, 25);
+            label11.TabIndex = 9;
+            label11.Text = "Total Staff";
+            // 
+            // panel3
+            // 
+            panel3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel3.BackColor = Color.White;
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(15, 11);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(219, 370);
+            panel3.TabIndex = 2;
+            panel3.Paint += panel3_Paint;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.White;
+            label7.Font = new Font("Times New Roman", 8F, FontStyle.Bold);
+            label7.ForeColor = Color.FromArgb(0, 0, 64);
+            label7.Location = new Point(70, 150);
+            label7.Name = "label7";
+            label7.Size = new Size(65, 19);
+            label7.TabIndex = 7;
+            label7.Text = "Director";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.White;
+            label6.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(0, 0, 64);
+            label6.Location = new Point(28, 118);
+            label6.Name = "label6";
+            label6.Size = new Size(160, 25);
+            label6.TabIndex = 6;
+            label6.Text = "Ch. Zafar Iqbal";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(139, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(275, 25);
+            label4.TabIndex = 7;
+            label4.Text = "Allama Iqbal Public School ";
+            label4.Click += label4_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox7.BackgroundImage = Properties.Resources.profile;
+            pictureBox7.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox7.Location = new Point(763, 9);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(27, 26);
+            pictureBox7.TabIndex = 2;
+            pictureBox7.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 0, 64);
+            ClientSize = new Size(800, 446);
+            Controls.Add(label4);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox7);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load_1;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox5;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label4;
+        private PictureBox pictureBox6;
+        private Label label5;
+        private Panel panel8;
+        private Panel panel7;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel3;
+        private Label label7;
+        private Label label6;
+        private PictureBox pictureBox7;
+        private Label label8;
+        private Label label12;
+        private Label label10;
+        private Label label9;
+        private Label label11;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DB_Final_Project
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,      // x-coordinate oef upper-left corner
+            int nTopRect,       // y-coordinate of upper-left corner
+            int nRightRect,     // x-coordinate of lower-right corner
+            int nBottomRect,    // y-coordinate of lower-right corner
+            int nWidthEllipse,  // height of ellipse
+            int nHeightEllipse  // width of ellipse
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, panel1.Width, panel1.Height, 20, 20));
+            panel2.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, panel2.Width, panel2.Height, 20, 20));
+            panel3.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, panel3.Width, panel3.Height, 20, 20));
+            panel5.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, panel5.Width, panel5.Height, 20, 20));
+            panel4.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, panel4.Width, panel3.Height, 20, 20));
+            panel6.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, panel6.Width, panel3.Height, 20, 20));
+
+            //SetButtonRoundedRegion(button1, 20, 20);
+            //SetButtonRoundedRegion(button2, 20, 20);
+            //SetButtonRoundedRegion(button3, 20, 20);
+            //SetButtonRoundedRegion(button4, 20, 20);
+
+            // Create a circular region for pictureBox2
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, pictureBox2.Width, pictureBox2.Height);
+            pictureBox2.Region = new Region(path);
+        }
+
+        private void SetButtonRoundedRegion(Button button, int ellipseWidth, int ellipseHeight)
+        {
+            button.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, button.Width, button.Height, ellipseWidth, ellipseHeight));
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel8_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
