49b34442042e04e19800b90e88085637a092d8b1
Muhiu Din <muhiudin005@gmail.com>
2025-03-30 00:19:55 +0500

    initiated working on admin panel

---
+++ adminDashboard.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace DB_Final_Project
+{
+    partial class adminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(adminDashboard));
+            label4 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            label7 = new Label();
+            label6 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            panel1 = new Panel();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label8 = new Label();
+            pictureBox1 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            button5 = new Button();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(127, 17);
+            label4.Name = "label4";
+            label4.Size = new Size(327, 25);
+            label4.TabIndex = 11;
+            label4.Text = "Allama Iqbal Public High School ";
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            panel2.BackColor = Color.FromArgb(224, 224, 224);
+            panel2.Controls.Add(dataGridView2);
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(panel3);
+            panel2.Location = new Point(127, 47);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(670, 395);
+            panel2.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(15, 11);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(219, 370);
+            panel3.TabIndex = 2;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.White;
+            label7.Font = new Font("Times New Roman", 8F, FontStyle.Bold);
+            label7.ForeColor = Color.FromArgb(0, 0, 64);
+            label7.Location = new Point(80, 149);
+            label7.Name = "label7";
+            label7.Size = new Size(52, 19);
+            label7.TabIndex = 7;
+            label7.Text = "Admin";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.White;
+            label6.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(0, 0, 64);
+            label6.Location = new Point(28, 118);
+            label6.Name = "label6";
+            label6.Size = new Size(160, 25);
+            label6.TabIndex = 6;
+            label6.Text = "Ch. Zafar Iqbal";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = Properties.Resources.profile;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(39, 15);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(134, 96);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pictureBox7.BackgroundImage = Properties.Resources.profile;
+            pictureBox7.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox7.Location = new Point(767, 16);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(27, 26);
+            pictureBox7.TabIndex = 10;
+            pictureBox7.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Font = new Font("Segoe UI Semibold", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            panel1.Location = new Point(3, 7);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(121, 437);
+            panel1.TabIndex = 8;
+            // 
+            // button4
+            // 
+            button4.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button4.BackColor = Color.FromArgb(0, 0, 64);
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Times New Roman", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(0, 236);
+            button4.Name = "button4";
+            button4.RightToLeft = RightToLeft.No;
+            button4.Size = new Size(107, 30);
+            button4.TabIndex = 11;
+            button4.Text = " Student ";
+            button4.TextAlign = ContentAlignment.MiddleRight;
+            button4.UseMnemonic = false;
+            button4.UseVisualStyleBackColor = true;
+            button4.UseWaitCursor = true;
+            // 
+            // button3
+            // 
+            button3.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button3.BackColor = Color.FromArgb(0, 0, 64);
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Times New Roman", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(0, 200);
+            button3.Name = "button3";
+            button3.RightToLeft = RightToLeft.No;
+            button3.Size = new Size(118, 30);
+            button3.TabIndex = 10;
+            button3.Text = "Salaries   ";
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseMnemonic = false;
+            button3.UseVisualStyleBackColor = true;
+            button3.UseWaitCursor = true;
+            // 
+            // button2
+            // 
+            button2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button2.BackColor = Color.FromArgb(0, 0, 64);
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Image = Properties.Resources.Logout__1_;
+            button2.ImageAlign = ContentAlignment.MiddleRight;
+            button2.Location = new Point(0, 408);
+            button2.Name = "button2";
+            button2.RightToLeft = RightToLeft.Yes;
+            button2.Size = new Size(118, 27);
+            button2.TabIndex = 9;
+            button2.Text = "Logout";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseMnemonic = false;
+            button2.UseVisualStyleBackColor = true;
+            button2.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            button1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button1.BackColor = Color.FromArgb(0, 0, 64);
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Times New Roman", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.MiddleRight;
+            button1.Location = new Point(3, 137);
+            button1.Name = "button1";
+            button1.RightToLeft = RightToLeft.Yes;
+            button1.Size = new Size(104, 26);
+            button1.TabIndex = 0;
+            button1.Text = "   Home     ";
+            button1.TextAlign = ContentAlignment.TopLeft;
+            button1.UseMnemonic = false;
+            button1.UseVisualStyleBackColor = true;
+            button1.UseWaitCursor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Elephant", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = SystemColors.ButtonHighlight;
+            label8.Location = new Point(17, 88);
+            label8.Name = "label8";
+            label8.Size = new Size(90, 29);
+            label8.TabIndex = 8;
+            label8.Text = "Admin";
+            label8.Click += label8_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.LOGO_removebg_preview;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(12, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(95, 76);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(274, 11);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(363, 168);
+            dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(274, 196);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.Size = new Size(363, 185);
+            dataGridView2.TabIndex = 4;
+            // 
+            // button5
+            // 
+            button5.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button5.BackColor = Color.FromArgb(0, 0, 64);
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Font = new Font("Times New Roman", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.ImageAlign = ContentAlignment.MiddleRight;
+            button5.Location = new Point(0, 169);
+            button5.Name = "button5";
+            button5.RightToLeft = RightToLeft.Yes;
+            button5.Size = new Size(121, 25);
+            button5.TabIndex = 12;
+            button5.Text = "Personal    ";
+            button5.UseMnemonic = false;
+            button5.UseVisualStyleBackColor = true;
+            button5.UseWaitCursor = true;
+            button5.Click += button5_Click;
+            // 
+            // adminDashboard
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 0, 64);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox7);
+            Controls.Add(panel1);
+            Name = "adminDashboard";
+            Text = "adminDashboard";
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label4;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label7;
+        private Label label6;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox7;
+        private Panel panel1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label8;
+        private PictureBox pictureBox1;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView1;
+        private Button button5;
+    }
+}
---
+++ adminDashboard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Final_Project
+{
+    public partial class adminDashboard : Form
+    {
+        public adminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            adminInfo adminInfoPage = new adminInfo();
+            adminInfoPage.Show();
+        }
+    }
+}
